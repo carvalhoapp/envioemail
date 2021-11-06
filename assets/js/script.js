e4d6ac075fee30d72075d67f8083cf03d7ac4210
Rafael Carvalho <54747564+rcarvalhocopiadora@users.noreply.github.com>
2021-11-06 12:59:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(function($){
 		event.preventDefault();
 
 		$.ajax({
-	    url: "https://formspree.io/your@email.com", 
+	    url: "https://formspree.io/f/rcarvalhocopiadora@gmail.com", 
 	    method: "POST",
 	    data: {
 	    	name: $("#name").val(),
